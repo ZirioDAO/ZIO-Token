555203c9b4db59069a656beb93d2577671228670
Jack Tsai <jtsai0606@gmail.com>
2023-07-11 07:31:53 -0400

    wip

--- contracts/ZioRewardPool.sol
+++ contracts/ZioRewardPool.sol
@@ -3,14 +3,34 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ZioRewardPool is ReentrancyGuard {
+contract ZioRewardPool is ReentrancyGuard, Ownable, Pausable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
     using Address for address;
 
+    /* ========== STATE VARIABLES ========== */
+
+    IERC20 public rewardsToken;
+    IERC20 public stakingToken;
+
     mapping(address => uint256) public rewards;
+
+    /* ========== CONSTRUCTOR ========== */
+
+    constructor(
+        address _owner,
+        address _rewardsDistribution,
+        address _rewardsToken,
+        address _stakingToken
+    ) public Ownable(_owner) {
+        rewardsToken = IERC20(_rewardsToken);
+        stakingToken = IERC20(_stakingToken);
+        rewardsDistribution = _rewardsDistribution;
+    }
 
     // address private stakeToken
     // constructor(Token _tokenAddress) {
